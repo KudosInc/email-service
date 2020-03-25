6c693ce2d581245ff5f3bdde727e88b79998d96f
Talal Arshad <talal@kudosnow.com>
2020-03-25 17:54:05 -0600

    uses https

--- src/request.js
+++ src/request.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const https = require('https');
 
 const callback = (res, resolve, reject) => {
   const buffers = [];
@@ -14,13 +14,13 @@ const callback = (res, resolve, reject) => {
 };
 
 const get = ({ headers, uri }) => new Promise((resolve, reject) => {
-  const req = http.request(uri, { headers, method: 'GET' }, (res) => callback(res, resolve, reject));
+  const req = https.request(uri, { headers, method: 'GET' }, (res) => callback(res, resolve, reject));
   req.on('error', reject);
   req.end();
 });
 
 const post = ({ headers, uri, body }) => new Promise((resolve, reject) => {
-  const req = http.request(uri, { headers, method: 'POST' }, (res) => callback(res, resolve, reject));
+  const req = https.request(uri, { headers, method: 'POST' }, (res) => callback(res, resolve, reject));
   req.on('error', reject);
   req.write(JSON.stringify(body));
   req.end();
